96632be058d9989e08a439be30b468a99407527d
Steve Calla <callasteven@gmail.com>
2022-10-24 18:17:29 -0600

    Comment out install

--- assets/js/install.js
+++ assets/js/install.js
@@ -1,17 +1,17 @@
-const installBtn = document.getElementById('installBtn');
-const textHeader = document.getElementById('textHeader');
+// const installBtn = document.getElementById('installBtn');
+// const textHeader = document.getElementById('textHeader');
 
-window.addEventListener('beforeinstallprompt', (event) => {
-  event.preventDefault();
-  installBtn.style.visibility = 'visible';
-  textHeader.textContent = 'Click the button to install!';
+// window.addEventListener('beforeinstallprompt', (event) => {
+//   event.preventDefault();
+//   installBtn.style.visibility = 'visible';
+//   textHeader.textContent = 'Click the button to install!';
 
-  installBtn.addEventListener('click', () => {
-    event.prompt();
-    installBtn.setAttribute('disabled', true);
-    installBtn.textContent = 'Installed!';
-  });
-});
+//   installBtn.addEventListener('click', () => {
+//     event.prompt();
+//     installBtn.setAttribute('disabled', true);
+//     installBtn.textContent = 'Installed!';
+//   });
+// });
 
 // window.addEventListener('appinstalled', (event) => {
 //   textHeader.textContent = 'Successfully installed!';
